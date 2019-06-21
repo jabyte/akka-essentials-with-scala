6b8682a669506cbb55afcdde6aa2e1119ab6da71
jabyte <32500012+jabyte@users.noreply.github.com>
2019-06-21 17:46:02 +0100

    Milestone

--- src/main/scala/VotingSystem.scala
+++ src/main/scala/VotingSystem.scala
@@ -21,23 +21,26 @@ object VotingSystem extends App {
 
     def voted(candidate: String): Receive = {
       case Vote(candidate) => context.become(voted(candidate))
-      case VoteStatusRequest => sender ! candidate
-    }
-
-    def voteStatus: Receive = {
-      case
+      case VoteStatusRequest => sender ! VoteStatusReply(Option[String](candidate))
     }
   }
 
   class VoteAggregator extends Actor {
 
-    override def receive: Receive = aggregateVotes
+    var candidatesAndVote = Tuple2[String, Int]
 
+    override def receive: Receive = aggregateVotes
 
     def aggregateVotes: Receive = {
       case AggregateVotes(citizens) =>
         citizens.map(c => c ! VoteStatusRequest)
       case VoteStatusReply(candidate) =>
+        val votes = candidate.get match {
+          case "Martin" => println(candidatesAndVote)
+          case "Jonas" => candidatesAndVote.tupled("Jonas", 0)
+          case "Roland" =>
+        }
+        sender ! votes
     }
   }
 
